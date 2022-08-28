d47a255b712d80ac223df666813e4d4f906d0923
temp-admin <drafff@gmail.com>
2022-08-28 23:42:59 +0300

    Hello world with Lua added

---
+++ initialize.lua
@@ -0,0 +1,4 @@
+local LOOT_NAVIGATOR_ADDON_CHANNEL = 'LootNav'
+
+SendAddonMessage(LOOT_NAVIGATOR_ADDON_CHANNEL, "Turtle WoW: Loot Navigator", "PARTY")
+DEFAULT_CHAT_FRAME:AddMessage('Turtle WoW: Loot Navigator loaded')
