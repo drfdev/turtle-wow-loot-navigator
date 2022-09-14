99801ab0a14d9970a0e49783f562acd7ba4824fe
temp-admin <drafff@gmail.com>
2022-09-14 22:27:44 +0300

    added new bosses

--- TurtleWoWLootNavigator/data/wbosses.lua
+++ TurtleWoWLootNavigator/data/wbosses.lua
@@ -4,6 +4,7 @@
 
 -- list:
 
+-- Concavius (63, loot: 65)
 -- Dark Reaver of Karazhan (63, loot: 66)
 -- Azuregos (63, loot: 66-71)
 -- Lord Kazzak (63, loot: 66-71)
@@ -12,3 +13,4 @@
 -- Taerar (63, loot: 71-72)
 -- Ysondre (63, loot: 71-72)
 -- Nerubian Overseer (63, loot: 75)
+-- Ostarius (63, loot: 75)
